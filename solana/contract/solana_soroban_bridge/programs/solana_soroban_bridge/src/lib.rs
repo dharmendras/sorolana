e0396e756e8aa435b42a9546f86ec0d506159d45
ZafarMansoori <zafar.mansoori@imentus.com>
2023-06-27 23:45:01 +0530

    Dipost Method and Deposit Event

--- solana/contract/solana_soroban_bridge/programs/solana_soroban_bridge/src/lib.rs
+++ solana/contract/solana_soroban_bridge/programs/solana_soroban_bridge/src/lib.rs
@@ -1,15 +1,103 @@
 use anchor_lang::prelude::*;
+use anchor_lang::{
+   
+    solana_program::{program::invoke, system_instruction},
+    
+    };
+    use anchor_spl::token;
+    // use anchor_spl::{
+    //     token::{Token,MintTo , Mint, mint_to },
+    // };
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("9inMRAuig1bbuLXbLXTEpTG4Km5aCU3DctamwhjH5fQ5");
 
 #[program]
 pub mod solana_soroban_bridge {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+   pub const  current_validators :[&str; 4]= ["SDFTLC3274HR57LB7JTFG6EQBZRUE5T5ICVEPCCABADUGQS7HZVKGRDX" ,
+                                 "5tCpncznFkwTvauTxjyuHj6E2Eef86bv8coicC1gnvhu" ,
+                                 "3FArMmrnhs4r2NpGaBkugfrz6MxDSnbv9sNoAcvPuwkX" ,
+                                  "7BLoSwV7MSFA66krJztzut7rhy8mXmCyk5Hnnm2S2k7"];
+
+    pub fn deposite(ctx: Context<Deposit> , amount: u64) -> Result<()> {
+
+        
+        let from_account = &ctx.accounts.from;
+        let to_account = &ctx.accounts.to;
+
+        //  transfer instruction
+       let transfer_instruction = system_instruction::transfer(
+        from_account.key , 
+       to_account.key , 
+       amount);
+
+       // invoke the  transfer  instruction
+        invoke( 
+        &transfer_instruction,
+        &[ from_account.to_account_info(),
+          to_account.to_account_info(),
+          ],
+        
+         
+        );
+        emit!(DepositEvent {
+            amount: 12,
+            token_address: "9ZJdKLk57wS3NDPqHNybYX8aoApzZXmRhCKzZ3fLhZpo".to_string(),
+            token_chain: 123,
+            to: "ALeMyZqeNSzZKTrcwHU1EJnBkxTnWXHc22iCvhyzZ3f".to_string(),
+            to_chain: 456,
+            fee: 100,
+        });
         Ok(())
     }
+
+    pub fn claim(ctx: Context<Claim> , value: String) -> Result<()>  { 
+        msg!("solana55{}" , value);
+        
+        
+            for  i in 0..current_validators.len() { 
+                
+                if  current_validators[i] == value{ 
+                    msg!("chal ja");
+                    break;
+                }
+                else { 
+                    msg!("nhi chala ")
+                }
+             }
+      
+        Ok(())
+    }
+   
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Deposit<'info> { 
+
+   #[account(mut)]
+   pub from: Signer<'info>,
+   #[account(mut)]
+   /// CHECK:` doc comment explaining why no checks through types are necessary
+   pub to : AccountInfo<'info>,
+   pub system_program: Program<'info, System>,
+}
+#[derive(Accounts)]
+pub struct Claim { 
+
+  }
+
+
+
+
+#[event]
+pub struct DepositEvent{ 
+    pub amount:u16,
+
+    #[index]
+    pub  token_address: String,
+    pub token_chain: u16,
+    pub to: String,
+    pub to_chain: u16,
+    pub fee: u16,
+}
--- solana/contract/solana_soroban_bridge/tests/solana_soroban_bridge.ts
+++ solana/contract/solana_soroban_bridge/tests/solana_soroban_bridge.ts
@@ -1,16 +1,56 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SolanaSorobanBridge } from "../target/types/solana_soroban_bridge";
-
+import  BN from 'bn.js'
+import fs from "fs";
+import { clusterApiUrl, Connection, Keypair, LAMPORTS_PER_SOL,PublicKey, Transaction,SYSVAR_RENT_PUBKEY, MAX_SEED_LENGTH } from '@solana/web3.js';
+// import { TOKEN_PROGRAM_ID , MINT_SIZE , createAssociatedTokenAccountInstruction ,
+//          createInitializeMintInstruction , getAssociatedTokenAddress} from "@solana/spl-token";
+import { publicKey } from "@coral-xyz/anchor/dist/cjs/utils";
+const con = new Connection("http://127.0.0.1:8899");
+//https://api.testnet.solana
+//http://127.0.0.1:8899
+const fromWallet = anchor.web3.Keypair.generate()
+const mint = anchor.web3.Keypair.generate()
 describe("solana_soroban_bridge", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const newAccountKP1 = anchor.web3.Keypair.fromSecretKey(new Uint8Array(JSON.parse(fs.readFileSync('/home/dell/imentus_project/sorolana/solana/contract/solana_soroban_bridge/tests/key.json').toString())));
+ // const newAccountKP2 = anchor.web3.Keypair.fromSecretKey(new Uint8Array(JSON.parse(fs.readFileSync('/home/dell/imentus_project/sorolana/solana/contract/soroban-soloana-bridge/tests/key2.json').toString())));
+  
+  //anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+   anchor.setProvider(provider)
+  
+  async function myFunction() {
+    // Some asynchronous operation that returns a promise
+    console.log("key 17 " , newAccountKP1.publicKey)
+    // Code here will wait for the promise to resolve before continuing
+          let persion1  = await con.getBalance(newAccountKP1.publicKey);
+          console.log("person one " , persion1)
+
+          // let persion2 = await con.getBalance(newAccountKP2.publicKey)
+          // console.log("person two " , persion2)
+  }
+  myFunction()
 
   const program = anchor.workspace.SolanaSorobanBridge as Program<SolanaSorobanBridge>;
-
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    let listener = null;
+
+    const transferAmount = new BN(1000000);
+  
+      const tx = await program.methods.deposite(transferAmount).accounts({ 
+           from: newAccountKP1.publicKey,
+           to : "2zkUob2Rnac9RM3tJwHmeqoabHvSpfKo68515XDSRu57",
+           systemProgram: anchor.web3.SystemProgram.programId
+      }).signers([newAccountKP1]).rpc();
+      console.log("tx" , tx)
+      
+    await program.methods.claim("SDFTLC3274HR57LB7JTFG6EQBZRUE5T5ICVEPCCABADUGQS7HZVKGRDX").rpc()
+   
+
   });
 });
+//9992000000
+//B7q7oBUuSH63pjofHzXoFoesjUcRVAyAYyMiCoTRcxhL
